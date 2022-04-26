50fdbcdcea2076016edddff9c50aa5db77ce0fec
rudgerylopes94 <63984559+rudgerylopes94@users.noreply.github.com>
2022-04-26 17:59:24 +0100

    First additions

---
+++ FinaProjectAppFood/Countries.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinaProjectAppFood
+{
+   public class Countries
+    {
+
+        //properties
+        public string CountryName { get; set; }
+
+        public string  TraditionalDish { get; set; }
+
+        public string  Continent { get; set; }
+
+
+
+
+        //constructors
+
+        //methods
+
+    }
+}
